39f49996f2f851e12869d2de12f261acff7244e9
bob-skywalker <q463250938@gmail.com>
2023-06-20 12:09:29 -0700

    ContentView Functionality & Style Implemented. Prepare to create different branches for detailView functionalities

--- Fetch-iOS-Coding-Challenge/Views/ContentView.swift
+++ Fetch-iOS-Coding-Challenge/Views/ContentView.swift
@@ -6,27 +6,73 @@
 //
 
 import SwiftUI
-import Kingfisher
+import Kingfisher //Kingfisher is used for image caching on user's device
 
 struct ContentView: View {
     @StateObject var mealViewModel = MealViewModel()
     
-    let columns = [
-        GridItem(.adaptive(minimum: 140))
-    ]
     
     var body: some View {
-        NavigationView {
-            ScrollView(.vertical){
-                LazyVGrid(columns: columns) {
-                    ForEach(mealViewModel.desserts) { dessert in
-                        Text(dessert.strMeal)
+        GeometryReader { geo in
+            NavigationView {
+                ScrollView(.vertical){
+                    VStack{
+                        ForEach(mealViewModel.desserts) { dessert in
+                            HStack{
+                                KFImage(URL(string: dessert.strMealThumb))
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                    .cornerRadius(10)
+                                    .overlay(
+                                        RoundedRectangle(cornerRadius: 10)
+                                            .stroke(Color.gray, lineWidth: 1)
+                                            .shadow(color: .black, radius:4)
+                                    )
+                                Spacer()
+                                
+                                VStack(alignment: .trailing, spacing: 10){
+                                    HStack{
+                                        Image(systemName: "clock.badge")
+                                            .resizable()
+                                            .scaledToFit()
+                                            .frame(width: 13, height: 13)
+                                        Text("3d")
+                                            .font(Font.caption)
+                                    }
+                                    .foregroundColor(.red).bold()
+                                    
+                                    Text(dessert.strMeal)
+                                        .font(Font.headline)
+                                    HStack{
+                                        ForEach(0..<3) { _ in
+                                            Image(systemName: "star.circle.fill")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 20, height: 20)
+                                                .foregroundColor(.orange)
+                                        }
+                                    }
+                                }
+                                //                                .frame(maxWidth: geo.size.width * 0.45)
+                                
+                            }
+                            .frame(maxWidth: .infinity)
+                            .frame(height: geo.size.height * 0.2)
+                            .padding(.horizontal, 10)
+                            .background(.white)
+                            .cornerRadius(10)
+                            .shadow(color: .black, radius: 4, x: 0, y: 2)
+                            .padding()
+                        }
                     }
                 }
+                .onAppear(perform: mealViewModel.fetchDesserts)
+                .navigationTitle("Discover Meals")
+                
             }
-            .onAppear(perform: mealViewModel.fetchDesserts)
         }
-
+        
     }
 }
 
