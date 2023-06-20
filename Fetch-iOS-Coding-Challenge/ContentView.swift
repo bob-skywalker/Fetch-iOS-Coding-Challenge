eea43e26639aee82338669d653c3a53ba33173b5
bob-skywalker <q463250938@gmail.com>
2023-06-20 11:01:00 -0700

    Initial Commit

---
+++ Fetch-iOS-Coding-Challenge/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Fetch-iOS-Coding-Challenge
+//
+//  Created by bo zhong on 6/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Fetch-iOS-Coding-Challenge/Fetch_iOS_Coding_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_Coding_ChallengeApp.swift
+//  Fetch-iOS-Coding-Challenge
+//
+//  Created by bo zhong on 6/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_Coding_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
